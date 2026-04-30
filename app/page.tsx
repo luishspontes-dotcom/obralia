bf4ab91752b6964848e57afc8cb51662003f9eff
luishspontes-dotcom <luishspontes@gmail.com>
2026-04-30 02:00:03 -0300

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { createServerSupabase } from "@/lib/supabase/server";
-
-export default async function RootPage() {
-  const supabase = await createServerSupabase();
-  const { data: { user } } = await supabase.auth.getUser();
-  if (user) redirect("/inicio");
-  redirect("/login");
-}
